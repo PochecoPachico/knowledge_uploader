0af11b8a2e8a1d3eb4b32f5363343a32769304cd
PochecoPachico <sasimi35@gmail.com>
2017-03-04 21:33:54 +0900

    implement file upload function

--- app/Controller/KnowledgesController.php
+++ app/Controller/KnowledgesController.php
@@ -1,6 +1,34 @@
 <?php
 class KnowledgesController extends AppController {
-  public function index() {
-    $this->set("data", $this->Knowledge->find("all"));
+  public function index($id) {
+    $conditions = array(
+      "Knowledge.task_id" => $id
+    );
+    $this->set("data", $this->Knowledge->find("first", array(
+          "conditions" => $conditions
+        )
+    ));
+  }
+
+  public function register() {
+    $this->autoRender = false;
+    if ($this->request->is("post")) {
+      $post_param = $this->request->data;
+      $upload_dir = "/Applications/MAMP/htdocs/knowledge_files/" . $post_param["Knowledge"]["task_id"] . "/"; 
+      $this->_createDirectory($upload_dir);
+      if (is_uploaded_file($post_param["Knowledge"]["file"]["tmp_name"])) {
+        if (move_uploaded_file($post_param["Knowledge"]["file"]["tmp_name"], $upload_dir . $post_param["Knowledge"]["file"]["name"])) {
+          echo "Success";
+        } else {
+          echo "Failed";
+        }
+      }
+    }
+  }
+
+  // 存在しなければディレクトリを作成
+  protected function _createDirectory($dir) {
+    if (!file_exists($dir))
+      mkdir($dir);
   }
 }
--- app/View/Knowledges/index.ctp
+++ app/View/Knowledges/index.ctp
@@ -1,3 +1,11 @@
 <?php
 pr($data);
 ?>
+
+<?php
+echo $this->Form->create("Knowledge", array(
+  "url" => array("controller" => "Knowledges", "action" => "register"),
+  "type" => "file"));
+echo $this->Form->hidden("task_id", array("value" => $data["Knowledge"]["task_id"]));
+echo $this->Form->input("file", array("type" => "file"));
+echo $this->Form->end("submit");
